88a0ed01bec8130569a1cfd030bb2be10a033490
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-09-17 18:07:59 +0100

    files setup

--- dist/server.js
+++ dist/server.js
@@ -13,6 +13,10 @@ var _dotenv = _interopRequireDefault(require("dotenv"));
 
 var _morgan = _interopRequireDefault(require("morgan"));
 
+var _webPush = _interopRequireDefault(require("web-push"));
+
+var _path = _interopRequireDefault(require("path"));
+
 var _bodyParser = require("body-parser");
 
 var _port = require("./port");
@@ -20,6 +24,7 @@ var _port = require("./port");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = (0, _express.default)();
+app.use(_express.default.static(_path.default.join(__dirname, "client")));
 
 _dotenv.default.config();
 
@@ -30,6 +35,20 @@ app.use((0, _morgan.default)("dev"));
 app.use((0, _bodyParser.urlencoded)({
   extended: true
 }));
+const publicVapidKey = "BGjj11BqxvyQBVjCJv0re2RQCraaFP3vGvwaA6nP4Tt65vgNjfF7Voy24EpYMswSYCMf6eiNIRfS2hk1n92-_wY";
+const privateVapidKey = "kI_phBopIui4lhfUvzvkh2u8ly4k1Y53XGHsG3jH0jA";
+
+_webPush.default.setVapidDetails("mailto:test@test.com", publicVapidKey, privateVapidKey);
+
+app.post("/notify", (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({
+    title: "Push Test"
+  });
+
+  _webPush.default.sendNotification(subscription, payload).catch(err => console.err(err));
+});
 
 const start = async () => {
   try {
--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import webpush from "web-push";
+import path from "path";
 import { json, urlencoded } from "body-parser";
 import { port } from "./port";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "client")))
 
 dotenv.config();
 
@@ -15,6 +19,28 @@ app.use(cors());
 app.use(json());
 app.use(morgan("dev"));
 app.use(urlencoded({ extended: true }));
+
+const publicVapidKey =
+  "BGjj11BqxvyQBVjCJv0re2RQCraaFP3vGvwaA6nP4Tt65vgNjfF7Voy24EpYMswSYCMf6eiNIRfS2hk1n92-_wY";
+const privateVapidKey = "kI_phBopIui4lhfUvzvkh2u8ly4k1Y53XGHsG3jH0jA";
+
+webpush.setVapidDetails(
+  "mailto:test@test.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
+app.post("/notify", (req, res) => {
+  const subscription = req.body;
+
+  res.status(201).json({});
+
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  webpush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.err(err));
+});
 
 export const start = async () => {
   try {
